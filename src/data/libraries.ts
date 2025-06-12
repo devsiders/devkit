45a375e37964fcbefd8ea76d6db6205c4461dd48
Carlos Torres <carlostorrez1258@gmail.com>
2025-06-12 21:46:39 +0000

    chore(data): update libraries file with popular CSS frameworks

--- src/data/libraries.ts
+++ src/data/libraries.ts
@@ -3,15 +3,6 @@ import { DevKit } from "../types/devKits";
 export const devKits: DevKit[] = [
   {
     id: '1',
-    name: 'Tailwind CSS',
-    description: 'Framework CSS utility-first para desarrollo rápido',
-    logo: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
-    url: 'https://tailwindcss.com',
-    category: 'libraries',
-    tags: ['css', 'styling', 'utility']
-  },
-  {
-    id: '2',
     name: 'Bootstrap',
     description: 'Framework CSS popular para construir interfaces responsive y modernas',
     logo: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png',
@@ -19,4 +10,64 @@ export const devKits: DevKit[] = [
     category: 'libraries',
     tags: ['css', 'responsive', 'framework']
   },
+  {
+    id: '2',
+    name: 'Bulma',
+    description: 'Framework CSS moderno basado en Flexbox.',
+    logo: 'https://bulma.io/images/bulma-logo.png',
+    url: 'https://bulma.io',
+    github: 'https://github.com/jgthms/bulma',
+    category: 'libraries',
+    tags: ['css', 'flexbox', 'responsive', 'framework']
+  },
+  {
+    id: '3',
+    name: 'Foundation',
+    description: 'Framework responsive avanzado para construir sitios web y correos HTML.',
+    logo: 'https://get.foundation/assets/img/foundation-logo.svg',
+    url: 'https://get.foundation',
+    github: 'https://github.com/foundation/foundation-sites',
+    category: 'libraries',
+    tags: ['css', 'responsive', 'framework']
+  },
+  {
+    id: '4',
+    name: 'Materialize',
+    description: 'Framework CSS basado en Material Design de Google.',
+    logo: 'https://materializecss.com/res/materialize.svg',
+    url: 'https://materializecss.com',
+    github: 'https://github.com/Dogfalo/materialize',
+    category: 'libraries',
+    tags: ['css', 'material-design', 'responsive', 'framework']
+  },
+  {
+    id: '5',
+    name: 'Semantic UI',
+    description: 'Framework CSS basado en clases legibles y semánticas para construir interfaces elegantes.',
+    logo: 'https://semantic-ui.com/images/logo.png',
+    url: 'https://semantic-ui.com',
+    github: 'https://github.com/Semantic-Org/Semantic-UI',
+    category: 'libraries',
+    tags: ['css', 'semantic', 'responsive', 'framework']
+  },
+  {
+    id: '6',
+    name: 'Skeleton',
+    description: 'Un micro framework CSS para sitios web responsive y rápidos.',
+    logo: 'https://skeletonframework.github.io/img/skeleton-logo.png',
+    url: 'http://getskeleton.com',
+    github: 'https://github.com/dhg/Skeleton',
+    category: 'libraries',
+    tags: ['css', 'lightweight', 'responsive', 'framework']
+  },
+  {
+    id: '7',
+    name: 'Tailwind CSS',
+    description: 'Framework CSS utility-first para desarrollo rápido',
+    logo: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
+    url: 'https://tailwindcss.com',
+    category: 'libraries',
+    tags: ['css', 'utility', 'responsive','framework']
+  },
+  
 ];
