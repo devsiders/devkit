1bc3762d677f9aaf19ac76202fc2ebbdf59d1e7f
Carlos Torres <carlostorrez1258@gmail.com>
2025-06-12 16:42:17 +0000

    refactor(devkit): load kits from separated data files

---
+++ src/types/devKits.ts
@@ -0,0 +1,11 @@
+export interface DevKit {
+  id: string;
+  name: string;
+  description: string;
+  logo: string;
+  url: string;
+  github?: string;
+  category: 'libraries'| 'ui' | 'icons' | 'fonts'| 'tools';
+  tags: string[];
+}
+
