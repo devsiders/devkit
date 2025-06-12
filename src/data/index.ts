6df0829a2b3c6ea24146d6d112f308664dec07dd
Carlos Torres <carlostorrez1258@gmail.com>
2025-06-12 16:44:20 +0000

    feat(data): add index file to unify libraries, icons and fonts

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+import { devKits as libraries } from './libraries';
+import { devKits as ui } from './ui';
+import { devKits as icons } from './icons';
+import { devKits as fonts } from './fonts';
+import { devKits as tools } from './tools';
+
+export const allDevKits = [...fonts, ...icons, ...libraries, ...ui, ...tools];
