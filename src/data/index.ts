12b6731446bff95143a0e4a4ee35e4a598538c66
Carlos Torres <carlostorrez1258@gmail.com>
2025-06-21 16:48:53 +0000

    refactor: remove 'ui' import and usage from devKits list

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,6 @@
 import { devKits as libraries } from './libraries';
-import { devKits as ui } from './ui';
 import { devKits as icons } from './icons';
 import { devKits as fonts } from './fonts';
 import { devKits as tools } from './tools';
 
-export const allDevKits = [...fonts, ...icons, ...libraries, ...ui, ...tools];
+export const allDevKits = [...fonts, ...icons, ...libraries, ...tools];
