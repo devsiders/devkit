1a134dcb8fb088fe7b18aa2092d4c9d82944363a
Carlos Torres <93625227+devsiders@users.noreply.github.com>
2025-11-15 11:22:39 -0500

    feat: add no-code category to data seed

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,5 +2,6 @@ import { devKits as libraries } from './libraries';
 import { devKits as icons } from './icons';
 import { devKits as fonts } from './fonts';
 import { devKits as tools } from './tools';
+import { devKits as nocode } from './nocode';
 
-export const allDevKits = [...fonts, ...icons, ...libraries, ...tools];
+export const allDevKits = [...fonts, ...icons, ...libraries, ...tools, ...nocode];
---
+++ src/data/nocode.ts
@@ -0,0 +1,6 @@
+import { DevKit } from "../types/devKits";
+
+export const devKits: DevKit[] = [
+  
+
+];
