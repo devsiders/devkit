2455eb042314189ef7b47caa7d30ba383066a34e
Carlos Torres <93625227+devsiders@users.noreply.github.com>
2025-11-15 12:10:58 -0500

    data: add nocode data

--- src/data/nocode.ts
+++ src/data/nocode.ts
@@ -1,6 +1,14 @@
 import { DevKit } from "../types/devKits";
 
 export const devKits: DevKit[] = [
-  
+  {
+      id: '1',
+      name: 'Framer',
+      description: '',
+      logo: 'https://res.cloudinary.com/daxzhb62n/image/fetch/f_auto,q_auto,w_100/https://cdn.worldvectorlogo.com/logos/framer-1.svg',
+      url: 'https://www.framer.com/',
+      category: 'nocode',
+      tags: ['animation', 'motion', 'react']
+    },
 
 ];
